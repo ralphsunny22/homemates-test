ffa982808dd6b6c77481db700b0bd7367b8d0fa6
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 <ralphsunny114@gmail.com>
2024-06-08 20:00:51 +0100

    workflow1

---
+++ app/Http/Controllers/Web/V1/AuthController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers\Web\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\CentralLogics\Helpers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function register(Request $request){
+        
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'password' => ['required', Password::min(8)],
+        ], [
+            'name.required' => 'The name field is required.',
+            'email.required' => 'The email name field is required.',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['status' => false, 'errors' => Helpers::error_processor($validator)], 403);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = Auth::login($user);
+        return response()->json([
+            'status' => true,
+            'message' => 'User created successfully',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
+    //login
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+                'status' => 'success',
+                'user' => $user,
+                'authorisation' => [
+                    'token' => $token,
+                    'type' => 'bearer',
+                ]
+            ]);
+
+    }
+
+    //refresh
+    public function refresh()
+    {
+        return response()->json([
+            'status' => true,
+            'user' => Auth::user(),
+            'authorisation' => [
+                'token' => Auth::refresh(),
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+
+    //logout
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/web/v1/api.php
@@ -0,0 +1,11 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['namespace' => 'Web\V1'], function () {
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('register', 'AuthController@register');
+        Route::post('login', 'AuthController@login');
+    });
+    
+});
